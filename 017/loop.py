d20ce1d25dc5d85ef927902659e4593ddef0a7f7
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-03-18 17:00:11 +0330

    ...

---
+++ 017/loop.py
@@ -0,0 +1,31 @@
+# loops in python
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+
+# print(100)
+
+
+# for
+
+# collection => list of numbers , list of characters in a string , range(1,5), etc
+
+listOfNumbers = [23, 54, 67, 89, 34, 9]
+
+# for number in listOfNumbers:
+#     print(number * 2)
+
+
+myName = "Shaghayegh"
+
+# for character in myName:
+#     print(character)
+
+rangeOfNumbers = range(1, 10)
+
+for num in rangeOfNumbers:
+    print(num * num)
