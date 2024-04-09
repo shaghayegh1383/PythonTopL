374ff8198c8ce0a046d8eb036bf755b7eacdf093
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-04-09 17:25:20 +0330

    pip

---
+++ 052/module.py
@@ -0,0 +1,8 @@
+#pypi.org
+
+import termcolor2
+#print (termcolor2.colored('shaghayegh', color= 'red' ))
+
+#help (termcolor2)
+
+print (termcolor2.colored ('shaghayegh' , color = 'white' , on_color = 'on_magenta'))
