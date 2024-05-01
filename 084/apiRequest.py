ce832c9ccea3b1b641fb3b5b8d2c336e27ccd010
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-05-02 00:15:48 +0330

    API

--- 084/apiRequest.py
+++ 084/apiRequest.py
@@ -1,6 +1,6 @@
 import requests
 response = requests.get("https://jsonplaceholder.typicode.com/comments")
-#print(response.json())
+print(response.json())
 
 for data in response.json():
     print (data)
