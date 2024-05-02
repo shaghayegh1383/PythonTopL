54061638eca4e16bad4a4810eb639d5911335b75
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-05-03 00:44:19 +0330

    Tkinter (GUI)

---
+++ 087/TK.py
@@ -0,0 +1,10 @@
+# GUI => Graphic user interface
+
+from tkinter import *
+
+root = Tk()
+root.title ("new py GUI")
+
+Label = Label (root , text= "This is test lable")
+Label.place(x=10 , y=10)
+root.mainloop()
